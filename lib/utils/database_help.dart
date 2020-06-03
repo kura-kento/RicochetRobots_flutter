2548aaeadf64710a7bb117c35d46682fc4bbfb26
けんと <knt.krmt@gmail.com>
2020-06-04 02:26:17 +0900

    stage add

--- lib/utils/database_help.dart
+++ lib/utils/database_help.dart
@@ -46,20 +46,27 @@ class DatabaseHelper {
 
     await db.execute('CREATE TABLE $tableName($colId INTEGER PRIMARY KEY AUTOINCREMENT, $colSize INTEGER,'
         '$colName TEXT, $colParameter TEXT, $colRobots TEXT, $colLock TEXT)');
-    await db.insert(tableName,Stage(5,"stage1",[[1,4,770],[1,3,14]],[[1,1]],false).toMap());
-    await db.insert(tableName,Stage(5,"stage1",[[2,2,210],[3,3,231],[1,3,14]],[[1,1]],true).toMap());
-    await db.insert(tableName,Stage(5,"stage2",[[2,2,210],[3,3,231]],[[4,0],[4,1]],true).toMap());
-    await db.insert(tableName,Stage(5,"stage2",[[2,3,462]],[[4,0],[4,1]],true).toMap());
-    await db.insert(tableName,Stage(5,"stage2",[[0,3,330],[1,1,21],[4,1,7],[1,4,3],[2,3,3]],[[0,0],[0,1]],true).toMap());
-    await db.insert(tableName,Stage(5,"stage2",[[1,3,11]],[[4,0],[4,1],[4,2]],true).toMap());
-    await db.insert(tableName,Stage(5,"stage2",[[0,3,770],[1,2,21],[4,1,7]],[[4,1],[4,4]],true).toMap());
-    await db.insert(tableName,Stage(5,"stage2",[[0,2,210],[2,1,210],[4,2,1155]],[[4,0],[4,1]],true).toMap());
-    await db.insert(tableName,Stage(6,"stage3",[[2,3,77],[3,0,3],[2,2,14],[3,5,6],[5,3,7],[0,2,7]],[[3,2],[4,1]],true).toMap());
-    await db.insert(tableName,Stage(6,"stage10",[[1,4,154],[1,2,15],[4,0,2],[4,2,14],[3,1,2],[3,4,2],[4,5,10]],[[5,0],[5,1]],false).toMap());
-    await db.insert(tableName,Stage(6,"stage3",[[4,1,231],[0,3,5],[3,3,15],[5,4,5],],[[3,3],[5,0]],false).toMap());
-    await db.insert(tableName,Stage(7,"stage3",[[3,4,10],[2,2,210],[1,1,231]],[[4,0],[4,1]],true).toMap());
-    await db.insert(tableName,Stage(7,"stage4",[[4,5,10],[2,2,210],[1,1,231]],[[4,0],[4,1]],true).toMap());
-
+    await db.insert(tableName,Stage(5,"stage1lv1",[[1,4,770],[1,3,14]],[[1,1]],false).toMap());
+    await db.insert(tableName,Stage(5,"stage1lv1",[[2,2,210],[3,3,231],[1,3,14]],[[1,1]],false).toMap());
+    await db.insert(tableName,Stage(5,"stage2lv5",[[2,2,210],[3,3,231]],[[4,0],[4,1]],false).toMap());
+    await db.insert(tableName,Stage(5,"stage2lv10",[[2,2,210],[4,0,2],[3,3,231],[4,1,7],[1,1,10]],[[1,4],[4,0]],false).toMap());
+    await db.insert(tableName,Stage(7,"stage3",[[1,5,770],[0,2,7],[2,2,21],[2,0,3],[0,4,5],[2,4,15],[2,6,3],[4,4,10],[6,4,5],[4,6,2],[4,0,2],[4,2,14],[6,2,7]],[[1,1],[5,1],[5,5]],false).toMap());
+    await db.insert(tableName,Stage(5,"stage2lv25",[[2,3,462]],[[4,0],[4,1]],false).toMap());
+    await db.insert(tableName,Stage(5,"stage2lv25",[[0,3,330],[1,1,21],[4,1,7],[1,4,3],[2,3,3]],[[0,0],[0,1]],false).toMap());
+    await db.insert(tableName,Stage(5,"stage2lv25",[[0,3,770],[1,2,21],[4,1,7]],[[4,1],[4,4]],false).toMap());
+    await db.insert(tableName,Stage(5,"stage2lv25",[[0,2,210],[2,1,10],[4,2,1155]],[[4,0],[4,1]],false).toMap());
+    await db.insert(tableName,Stage(6,"stage3lv25",[[2,3,77],[3,0,3],[2,2,14],[3,5,6],[5,3,7],[0,2,7]],[[3,2],[4,1]],false).toMap());
+    await db.insert(tableName,Stage(5,"stage2lv50",[[1,3,11]],[[4,0],[4,1],[4,2]],false).toMap());
+    await db.insert(tableName,Stage(5,"stage2lv60",[[2,2,11]],[[4,0],[4,1],[4,2]],false).toMap());
+    await db.insert(tableName,Stage(7,"stage3",[[2,2,110],[2,5,14],[4,5,10],[5,2,14],[5,0,2],[4,6,2]],[[0,5],[6,0],[6,6]],false).toMap());
+    await db.insert(tableName,Stage(6,"stage3lv60",[[4,1,231],[0,3,5],[3,3,15],[5,4,5],],[[3,3],[5,0]],false).toMap());
+    await db.insert(tableName,Stage(6,"stage3lv60",[[2,2,154],[0,0,7],[0,2,7],[2,1,2],[3,4,14],[4,2,10],[5,2,15],[5,3,7]],[[0,5],[3,2]],false).toMap());
+    await db.insert(tableName,Stage(5,"stage2lv80",[[2,1,11]],[[4,0],[4,1],[4,2]],false).toMap());
+    await db.insert(tableName,Stage(6,"stage10lv80",[[1,4,154],[1,2,15],[4,0,2],[4,2,14],[3,4,2],[4,5,10]],[[5,0],[5,1]],false).toMap());
+    await db.insert(tableName,Stage(5,"stage3",[[4,4,11]],[[4,1],[4,0]],false).toMap());
+    await db.insert(tableName,Stage(6,"stage3",[[5,5,11]],[[5,1],[5,0]],false).toMap());
+    await db.insert(tableName,Stage(7,"stage3",[[0,0,11]],[[4,0],[4,1]],false).toMap());
+    await db.insert(tableName,Stage(8,"stage3",[[0,0,11]],[[4,0],[4,1]],false).toMap());
   }
 
   // Fetch Operation: データベースからすべてのカレンダーオブジェクトを取得します
